1e8811cdfc1a4c5d2ca3312528f2f1676876b840
Luis Suarez <suarezluis@gmail.com>
2020-02-07 09:12:56 -0600

    Add more on functions and arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,17 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [['f150'], ['corolla'], ['sierra']];
+
+// Help with inference when extracting values
+
+const carMake = carMakers[0]; //inference string
+const myCar = carMakers.pop(); //inference string
+
+// Preventing incompatible values
+carMakers.push(100); // Argument of type '100' is not assignable to parameter of type 'string'
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toLocaleUpperCase();
+});
